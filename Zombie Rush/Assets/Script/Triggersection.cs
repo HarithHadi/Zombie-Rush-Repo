ab697e7b1e210e0f58ef00890a75cd8dc93d1e4e
aridontknowcode <m.harith.hadi@gmail.com>
2025-12-16 12:42:36 +0800

    Added 3d models and scripts

---
+++ Zombie Rush/Assets/Script/Triggersection.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triggersection : MonoBehaviour
+{
+    public GameObject roadSection;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Trigger"))
+        {
+            Instantiate(roadSection, new Vector3(-0, 0,  0), Quaternion.identity);
+        }
+    }
+}
